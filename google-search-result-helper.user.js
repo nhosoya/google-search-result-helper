c36efd105419063069a74626d4404f75eff82150
Naoki Hosoya <hosoya.naoki@moneyforward.co.jp>
2018-06-13 18:55:51 +0900

    Bump version to 0.2.4

--- google-search-result-helper.user.js
+++ google-search-result-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         google-search-result-helper
 // @namespace    https://github.com/nhosoya
-// @version      0.2.3
+// @version      0.2.4
 // @description  Shortcut(j, k, /) for Google search result
 // @author       nhosoya
 // @include      https://www.google.co.jp/search?*
